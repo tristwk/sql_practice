cae9c870b5dfc84b228bad87b93d8ebe494481a5
Trista Kuo <109693449+tristwk@users.noreply.github.com>
2022-11-13 14:46:35 +0100

    Create Department Top Three Salaries.sql
    https://leetcode.com/problems/department-top-three-salaries/

---
+++ Department Top Three Salaries.sql
@@ -0,0 +1,16 @@
+-- CTE: rank salary by department 
+
+WITH rank_table AS (
+SELECT e.name,
+       salary,
+       d.name AS department_name,
+       dense_rank () OVER (PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+FROM Employee e
+LEFT JOIN Department d 
+  ON e.departmentId = d.id  
+)
+SELECT department_name AS Department, 
+       name AS Employee, 
+       salary AS Salary
+FROM rank_table
+WHERE salary_rank <= 3;
