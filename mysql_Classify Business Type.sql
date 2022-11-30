ce9b87192fe07695e93a19bba0e7e773bcba47c4
Trista Kuo <109693449+tristwk@users.noreply.github.com>
2022-11-30 11:53:52 +0100

    Update mysql_Classify Business Type.sql

--- mysql_Classify Business Type.sql
+++ mysql_Classify Business Type.sql
@@ -6,3 +6,15 @@ WHEN business_name LIKE '%school%' THEN 'School'
 ELSE 'Other'
 END AS 'Business Type'
 from sf_restaurant_health_violations;
+
+
+
+-- postgresql 
+select business_name,
+CASE 
+WHEN lower(business_name) LIKE '%restaurant%' THEN 'restaurant' 
+WHEN lower(business_name) ~~ ANY('{%cafe%,%café%,%coffee%}') THEN 'cafe'
+WHEN lower(business_name) LIKE '%school%' THEN 'school'
+ELSE 'other'
+END 
+from sf_restaurant_health_violations;
